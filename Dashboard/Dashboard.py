6f24f2ce15327b4be3ee2c89198fbb58f4d67918
medhatfawzy <medhat-fawzy@outlook.com>
2022-12-17 17:43:51 +0200

    Dashboard improved and time range enabled

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -1,6 +1,7 @@
 import pandas as pd
 # The server and widgets library
 import panel as pn
+# enabling panel backend
 pn.extension()
 
 # the plotting libs
@@ -9,19 +10,22 @@ from holoviews.operation.datashader import inspect, rasterize
 from  holoviews.element.tiles import ESRI
 # provides the color maps
 import colorcet as cc
-# a module for plotting large ds
-import datashader as ds
-# from datashader.utils import lnglat_to_meters
+
+# using bokeh as a backend for holoviews, it works better than plotly wiith panel
 hv.extension('bokeh')
+#--------------------------------------------------
+
+# The options for the operators and thier colour maps
+COLORS = {"Operator A": cc.kr, "Operator B": cc.kg, "Operator C": cc.kb}
+PLOT_WIDTH = 900
+PLOT_HEIGHT= 900
 
 RSRP_data = pd.read_parquet("RSRP_data_viz.parq")
 
 # Using the map tiles provided by Esri. OpenStreetMaps can be used as well
-esri = ESRI().redim(x="Longtitude", y="Latitude").opts(alpha=0.2, width=900, height=700, bgcolor='black')
+esri = ESRI().redim(x="Longtitude", y="Latitude").opts(alpha=0.2, width=PLOT_WIDTH, height=PLOT_HEIGHT, bgcolor='black')
 # osm = OSM().redim(x="Longtitude", y="Latitude").opts(alpha=0.2, width=900, height=700, bgcolor='black')
 
-# The options for the operators and thier colour maps
-COLORS = {"Operator A": cc.kr, "Operator B": cc.kg, "Operator C": cc.kb}
 
 def hour_map(hours_range, days_range):
     start_hour, end_hour = hours_range
@@ -33,7 +37,7 @@ def hour_map(hours_range, days_range):
     if (len(df_time_range) == 0):
         return esri
     time_range_points = hv.Points(df_time_range, kdims=["LocationLongitude", "LocationLatitude"])
-    rastered = rasterize(time_range_points).opts(cmap=cc.fire, cnorm="eq_hist", width=900, height=700)
+    rastered = rasterize(time_range_points).opts(cmap=cc.fire, cnorm="eq_hist", width=PLOT_WIDTH, height=PLOT_HEIGHT)
     hour_highlight = inspect(rastered).opts(marker="o", size=10, fill_alpha=0, color='white', tools=["hover"])
     return esri * rastered * hour_highlight
 
@@ -41,8 +45,8 @@ def operator_map(operator_name):
     df_operator = RSRP_data[RSRP_data["RadioOperatorName"] == operator_name].copy()
     if (len(df_operator) == 0 ):
         return esri
-    operator_points =  hv.Points(df_operator, kdims=["LocationLongitude", "LocationLatitude"], aggregator=ds.count_cat('RadioOperatorName'))
-    rastered = rasterize(operator_points).opts(cmap=COLORS[operator_name], cnorm="eq_hist",  width=900, height=700)
+    operator_points =  hv.Points(df_operator, kdims=["LocationLongitude", "LocationLatitude"])
+    rastered = rasterize(operator_points).opts(cmap=COLORS[operator_name], cnorm="eq_hist",  width=PLOT_WIDTH, height=PLOT_HEIGHT)
     operator_highlight = inspect(rastered).opts(marker="o", size=10, fill_alpha=0, color='white', tools=["hover"])
     return esri * rastered * operator_highlight
 
@@ -57,7 +61,6 @@ Operators_maps = pn.bind(operator_map, operator_name=operator_select)
 template = pn.template.FastListTemplate(title="Telecom Analysis",
                                         main=[pn.Row(pn.Column(day_select, Hours_maps,hour_select), 
                                                     pn.Column(operator_select, Operators_maps))
-                                            ],
-
+                                            ]
 )
 template.servable()
