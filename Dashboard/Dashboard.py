410506d6b485f8511eb11a671d87ee2b75755c4a
medhatfawzy <medhat-fawzy@outlook.com>
2022-12-18 12:53:26 +0200

    More improvments and new notebook

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -26,12 +26,12 @@ RSRP_data = pd.read_parquet("RSRP_data_viz.parq")
 esri = ESRI().redim(x="Longtitude", y="Latitude").opts(alpha=0.2, width=PLOT_WIDTH, height=PLOT_HEIGHT, bgcolor='black')
 # osm = OSM().redim(x="Longtitude", y="Latitude").opts(alpha=0.2, width=900, height=700, bgcolor='black')
 
-def hour_map(hours_range, days_range):
+def time_range_plot(hours_range, days_range):
     start_hour, end_hour = hours_range
     start_day, end_day = days_range
-    df_time_range = RSRP_data[  ((start_day <= RSRP_data["Timestamp"].dt.day) & (RSRP_data["Timestamp"].dt.day<= end_day)) 
-                                    &
-                                ((start_hour <= RSRP_data["Timestamp"].dt.hour) & (RSRP_data["Timestamp"].dt.hour<= end_hour))].copy()
+    df_time_range = RSRP_data[((start_day <= RSRP_data["Timestamp"].dt.day) & (RSRP_data["Timestamp"].dt.day <= end_day)) 
+                                &
+                              ((start_hour <= RSRP_data["Timestamp"].dt.hour) & (RSRP_data["Timestamp"].dt.hour <= end_hour))].copy()
 
     if (len(df_time_range) == 0):
         return esri
@@ -41,7 +41,7 @@ def hour_map(hours_range, days_range):
     return esri * rastered * hour_highlight
 
 
-def operator_map(operator_name):
+def operator_plot(operator_name):
     df_operator = RSRP_data[RSRP_data["RadioOperatorName"] == operator_name].copy()
     if (len(df_operator) == 0 ):
         return esri
@@ -52,15 +52,16 @@ def operator_map(operator_name):
 
 hour_select = pn.widgets.IntRangeSlider(name="Hour", start=0, end=23, value=(2, 8))
 day_select = pn.widgets.IntRangeSlider(name="Day", start=1, end=4, value=(2,3))
-Hours_maps = pn.bind(hour_map, hours_range=hour_select, days_range=day_select)
+Time_range_map = pn.bind(time_range_plot, hours_range=hour_select, days_range=day_select)
 
 operator_select = pn.widgets.RadioButtonGroup(options=list(COLORS.keys()))
-Operators_maps = pn.bind(operator_map, operator_name=operator_select)
+Operators_maps = pn.bind(operator_plot, operator_name=operator_select)
 
 
 template = pn.template.FastListTemplate(title="Telecom Analysis",
-                                        main=[pn.Row(pn.Column(day_select, Hours_maps,hour_select), 
-                                                    pn.Column(operator_select, Operators_maps))
-                                            ]
-)
+                                        main=[pn.Row(pn.Column(day_select, Time_range_map, hour_select), 
+                                                     pn.Column(operator_select, Operators_maps)
+                                                    )
+                                             ]
+                                       )
 template.servable()
